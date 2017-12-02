97dc66dcedf962d9e9ecd55a2ada686c691dac62
juanb <juanbracamontes02@gmail.com>
2017-12-02 16:16:31 -0700

    error corregido

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,21 +4,20 @@ import {AngularFireDatabase} from "angularfire2/database";
 import {alertService} from "./alert.service";
 import {User} from "../interfaces/user";
 import {Globals} from "../statics/globals";
-import {_global} from "@angular/core/src/util";
+// import {_global} from "@angular/core/src/util";
 
 @Injectable()
 export class UserService {
 
     constructor(private db: AngularFireDatabase,
-                private alertService: alertService,
-                private _globals: Globals) {
+                private alertService: alertService) {
     }
 
     authUser(user: userAuthInterface) {
         return new Promise((resolve, reject) => {
             this.db.object('users/' + user.username).subscribe((result: User) => {
                 if (result.username != null) {
-                    this._globals.userInfo = result;
+                    // this._globals.userInfo = result;
                     resolve();
                 } else {
                     reject();
