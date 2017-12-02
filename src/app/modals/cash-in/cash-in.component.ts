97c8a74b52169d267296489bec43766643e0efff
DavidLio <liodavid0@gmail.com>
2017-12-01 18:29:46 -0700

    Creacion del diseño modal CashIn

--- src/app/modals/cash-in/cash-in.component.ts
+++ src/app/modals/cash-in/cash-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-cash-in',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CashInComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activeModal: NgbActiveModal) { }
 
   ngOnInit() {
+  }
+
+  closeModal(){
+    this.activeModal.close();
   }
 
 }
