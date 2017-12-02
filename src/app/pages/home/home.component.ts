5d99e2699b88873b4bd1bf05a9d1c02b5f0dfd07
Paola Gama <paolaggd@gamil.com>
2017-12-01 17:01:51 -0700

    - Creacion del diseño de los botones - Acomodo del diseño de la tabla al haber muchos registros (scroll) - Funcionalidad para abrir los modales correspondientes al dar click a un boton

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {CashInComponent} from "../../modals/cash-in/cash-in.component";
+import {CashOutComponent} from "../../modals/cash-out/cash-out.component";
+import {RefundComponent} from "../../modals/refund/refund.component";
+import {SignUpComponent} from "../../modals/sign-up/sign-up.component";
+import {CloseDateComponent} from "../../modals/close-date/close-date.component";
+import {window} from "rxjs/operator/window";
 
 @Component({
   selector: 'app-home',
@@ -7,10 +14,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
   }
 
+    openCashIn() {
+        this.modalService.open(CashInComponent, {backdrop: 'static', keyboard: false, size: "lg"});
+    }
 
+    openCashOut() {
+        this.modalService.open(CashOutComponent, {backdrop: 'static', keyboard: false, size: "lg"});
+    }
+
+    openRefund() {
+        this.modalService.open(RefundComponent, {backdrop: 'static', keyboard: false, size: "lg"});
+    }
+
+    openSignUp() {
+        this.modalService.open(SignUpComponent, {backdrop: 'static', keyboard: false, size: "lg"});
+    }
+
+    openCloseDate() {
+        this.modalService.open(CloseDateComponent, {backdrop: 'static', keyboard: false, size: "lg"});
+    }
+
+  //   open(){
+  //     window.document.write("<p>This is 'myWindow'</p>");
+  //   }
+  //   close(){
+  // window.close();
+  // }
 }
