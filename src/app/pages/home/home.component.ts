faadde0aa46d7089fbe4c78f6a02a42f8770b9c9
Paola Gama <paolaggd@gamil.com>
2017-12-01 17:01:51 -0700

    - Creacion del diseño de los botones - Acomodo del diseño de la tabla al haber muchos registros (scroll) - Funcionalidad para abrir los modales correspondientes al dar click a un boton

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,16 @@
-import {Component, OnInit} from '@angular/core';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
 import {InitialCashComponent} from "../../modals/initial-cash/initial-cash.component";
 import {Globals} from "../../statics/globals";
 import {LoginComponent} from "../../modals/login/login.component";
+import { Component, OnInit } from '@angular/core';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {CashInComponent} from "../../modals/cash-in/cash-in.component";
+import {CashOutComponent} from "../../modals/cash-out/cash-out.component";
+import {RefundComponent} from "../../modals/refund/refund.component";
+import {SignUpComponent} from "../../modals/sign-up/sign-up.component";
+import {CloseDateComponent} from "../../modals/close-date/close-date.component";
+import {window} from "rxjs/operator/window";
+
 
 @Component({
     selector: 'app-home',
@@ -11,13 +19,29 @@ import {LoginComponent} from "../../modals/login/login.component";
 })
 export class HomeComponent implements OnInit {
 
-    constructor(private _modal: NgbModal,
-                private _globals: Globals) {
-        _modal.open(InitialCashComponent, _globals.optionModalSm)
-      //this.modalService.open(LoginComponent,{backdrop: 'static', keyboard: false, size: "lg"});
-  }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
-
   }
+
+    openCashIn() {
+        this.modalService.open(CashInComponent, {backdrop: 'static', keyboard: false, size: "lg"});
+    }
+
+    openCashOut() {
+        this.modalService.open(CashOutComponent, {backdrop: 'static', keyboard: false, size: "lg"});
+    }
+
+    openRefund() {
+        this.modalService.open(RefundComponent, {backdrop: 'static', keyboard: false, size: "lg"});
+    }
+
+    openSignUp() {
+        this.modalService.open(SignUpComponent, {backdrop: 'static', keyboard: false, size: "lg"});
+    }
+
+    openCloseDate() {
+        this.modalService.open(CloseDateComponent, {backdrop: 'static', keyboard: false, size: "lg"});
+    }
+
 }
