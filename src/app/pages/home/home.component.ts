7c64a38c1b320f5b0f1ac7973216307bbfab3fc0
Carlos Alatorre <carlos_alatorre_95@hotmail.com>
2017-12-01 17:31:15 -0700

    configuracion inicial para initial cash

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {InitialCashComponent} from "../../modals/initial-cash/initial-cash.component";
+import {Globals} from "../../statics/globals";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    constructor(private _modal: NgbModal,
+                private _globals: Globals) {
+        _modal.open(InitialCashComponent, _globals.optionModalSm)
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 
 }
--- src/app/statics/globals.ts
+++ src/app/statics/globals.ts
@@ -1,6 +1,15 @@
 import {Injectable} from "@angular/core";
 
 @Injectable()
-export class Globals{
-    username: string = "carlos";
+export class Globals {
+    optionModalSm: object = {
+        backdrop: "static",
+        keyboard: false,
+        size: "sm"
+    };
+    optionModalLg: object = {
+        backdrop: "static",
+        keyboard: false,
+        size: "lg"
+    };
 }
