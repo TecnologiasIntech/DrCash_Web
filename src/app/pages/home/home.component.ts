863e808123613e5d619409e76d8a0aba3c78b240
DavidLio <liodavid0@gmail.com>
2017-12-05 15:14:17 -0700

    Se reducio el espacio entre el PatienName y el encabezado, se hicieron mas grandes las letras de los totales

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {InitialCashComponent} from "../../modals/initial-cash/initial-cash.component";
 import {Globals} from "../../statics/globals";
 import {LoginComponent} from "../../modals/login/login.component";
+import {CashInComponent} from "../../modals/cash-in/cash-in.component";
 
 @Component({
     selector: 'app-home',
@@ -13,8 +14,9 @@ export class HomeComponent implements OnInit {
 
     constructor(private _modal: NgbModal,
                 private _globals: Globals) {
-        // _modal.open(InitialCashComponent, _globals.optionModalSm)
-      _modal.open(LoginComponent,{backdrop: 'static', keyboard: false, size: "lg"});
+       /* _modal.open(InitialCashComponent, _globals.optionModalSm)*/
+        _modal.open(CashInComponent, _globals.optionModalLg)
+      //this.modalService.open(LoginComponent,{backdrop: 'static', keyboard: false, size: "lg"});
   }
 
   ngOnInit() {
