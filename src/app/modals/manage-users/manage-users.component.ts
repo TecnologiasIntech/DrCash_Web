90f898846baf1d6a7706d2363a4d3d884784d47a
DavidLio <liodavid0@gmail.com>
2017-12-21 14:55:49 -0700

    Se agrego la funcionalidad para guardar los cambios aplicados a un usuario, con sus respectivas validaciones

--- src/app/modals/manage-users/manage-users.component.ts
+++ src/app/modals/manage-users/manage-users.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {NgbActiveModal, NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {SignUpComponent} from "../sign-up/sign-up.component";
 import {Globals} from "../../statics/globals";
+import {UserService} from "../../services/user.service";
+import {User} from "../../interfaces/user";
+import {forEach} from "@angular/router/src/utils/collection";
+import {FirebaseListObservable} from "angularfire2/database-deprecated";
+import {ValidationService} from "../../services/validation.service";
 
 @Component({
   selector: 'app-manage-users',
@@ -11,10 +16,64 @@ import {Globals} from "../../statics/globals";
 export class ManageUsersComponent implements OnInit {
 
   constructor(private _activeModal: NgbActiveModal,
-              private _modal: NgbModal) { }
+              private _modal: NgbModal,
+              private _usersService: UserService) { }
 
   ngOnInit() {
+      this._usersService.getUsers().then(response=>{
+        this.users = response;
+        console.log(this.users);
+      })
   }
+
+  users;
+
+  editableUser: User = {} as User;
+  securityLevel: string;
+
+    @ViewChild('firstName')
+    firstName: ElementRef;
+    @ViewChild('lastName')
+    lastName: ElementRef;
+    @ViewChild('email')
+    email: ElementRef;
+
+  sendUserToEdit(  _user: User){
+    this.editableUser = _user;
+    this.securityLevel = this.editableUser.securityLevel.toString();
+    console.log(this.editableUser.securityLevel);
+  }
+
+    saveUser(){
+    debugger;
+        if(!this.areEmptyFields()){
+            this._usersService.updateUser(this.editableUser);
+            this.resetEditableUser();
+        }
+    }
+
+    areEmptyFields(){
+        if(ValidationService.errorInField(this.editableUser.firstName)){
+            this.firstName.nativeElement.focus();
+            return true;
+        }else{
+            if(ValidationService.errorInField(this.editableUser.lastName)){
+                this.lastName.nativeElement.focus();
+                return true;
+            }else{
+                if(ValidationService.errorInField(this.editableUser.email)){
+                    this.email.nativeElement.focus();
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        }
+    }
+
+    resetEditableUser(){
+        this.editableUser = {} as User;
+    }
 
   closeModal(){
     this._activeModal.close();
