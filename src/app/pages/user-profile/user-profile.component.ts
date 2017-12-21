f3f6b53b0a77b25a9a4aa7f2f0e84bcd016103e1
DavidLio <liodavid0@gmail.com>
2017-12-20 23:12:08 -0700

    Se agrego la funcionalidad para cargar los datos y las validaciones correspondientes para guardar el perfil editado

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../../interfaces/user";
+import {Globals} from "../../statics/globals";
+import {ValidationService} from "../../services/validation.service";
 
 @Component({
   selector: 'app-user-profile',
@@ -9,17 +12,80 @@ export class UserProfileComponent implements OnInit {
 
   constructor() { }
 
+  editableUser: User = Globals.userInfo;
+
+  newPassword: string;
+  confirmPassword: string;
+  showWarning = false;
+  warning: string;
+
   ngOnInit() {
   }
 
   editProfile: boolean = false;
+
+  areBasicFieldsEmpty(){
+    return(ValidationService.errorInField(this.editableUser.firstName) ||
+        ValidationService.errorInField(this.editableUser.lastName) ||
+        ValidationService.errorInField(this.editableUser.email))
+  }
+
+  isOnePasswordFieldEmptyButTheOtherOneNot(){
+    if(ValidationService.errorInField(this.newPassword)){
+      return (!ValidationService.errorInField(this.confirmPassword))
+    }
+    else{
+      return(ValidationService.errorInField(this.confirmPassword))
+    }
+  }
+
+  doThePasswordsMatch(){
+    return(this.newPassword == this.confirmPassword);
+  }
+
+  disableWarnings(){
+    this.showWarning = false;
+  }
+
+  showEmptyFieldsWarning(){
+    this.warning ="You must fill all te fields.";
+    this.showWarning = true;
+  }
+
+  showPasswordsDoNotMatchWarning(){
+    this.warning = "Passwords do not match";
+    this.showWarning = true;
+  }
+
+  areProfileFieldsReady(){
+    if(this.areBasicFieldsEmpty()){
+      this.showEmptyFieldsWarning();
+      return false;
+    }else{
+      if(this.isOnePasswordFieldEmptyButTheOtherOneNot()){
+        debugger;
+        this.showEmptyFieldsWarning();
+        return false
+      }
+      else{
+        if(this.doThePasswordsMatch()){
+          return true;
+        }else{
+          this.showPasswordsDoNotMatchWarning();
+        }
+      }
+    }
+  }
 
   cancelEditProfile(){
     this.editProfile = false;
   }
 
   finishEditProfile(){
-    this.editProfile = false;
+    if(this.areProfileFieldsReady()){
+      Globals.userInfo = this.editableUser;
+      this.editProfile = false;
+    }
   }
 
 }
