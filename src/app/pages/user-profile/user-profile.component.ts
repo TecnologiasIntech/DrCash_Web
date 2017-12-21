e3603e4469a1ced92ed2282bf0cc37033c946dd1
DavidLio <liodavid0@gmail.com>
2017-12-21 00:23:48 -0700

    Se agregaron las preguntas de seguridad, se agrego funcion para actualizar datos de seguridad

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,91 +1,117 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from "../../interfaces/user";
 import {Globals} from "../../statics/globals";
 import {ValidationService} from "../../services/validation.service";
+import {setTimeout} from "timers";
+import {UserService} from "../../services/user.service";
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss']
+    selector: 'app-user-profile',
+    templateUrl: './user-profile.component.html',
+    styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+    constructor(private _userService: UserService) {
+        // setTimeout(()=>{
+        // }, 5000)
+        this.editableUser = Globals.userInfo;
 
-  editableUser: User = Globals.userInfo;
-
-  newPassword: string;
-  confirmPassword: string;
-  showWarning = false;
-  warning: string;
-
-  ngOnInit() {
-  }
-
-  editProfile: boolean = false;
-
-  areBasicFieldsEmpty(){
-    return(ValidationService.errorInField(this.editableUser.firstName) ||
-        ValidationService.errorInField(this.editableUser.lastName) ||
-        ValidationService.errorInField(this.editableUser.email))
-  }
-
-  isOnePasswordFieldEmptyButTheOtherOneNot(){
-    if(ValidationService.errorInField(this.newPassword)){
-      return (!ValidationService.errorInField(this.confirmPassword))
     }
-    else{
-      return(ValidationService.errorInField(this.confirmPassword))
+
+    editableUser: User;
+
+    newPassword: string;
+    confirmPassword: string;
+    showWarning = false;
+    warning: string;
+    showSuccessAlert: boolean = false;
+
+    ngOnInit() {
     }
-  }
 
-  doThePasswordsMatch(){
-    return(this.newPassword == this.confirmPassword);
-  }
+    editProfile: boolean = false;
 
-  disableWarnings(){
-    this.showWarning = false;
-  }
+    areBasicFieldsEmpty() {
+        return (ValidationService.errorInField(this.editableUser.firstName) ||
+            ValidationService.errorInField(this.editableUser.lastName) ||
+            ValidationService.errorInField(this.editableUser.email) ||
+            ValidationService.errorInField(this.editableUser.securityAnswer))
+    }
 
-  showEmptyFieldsWarning(){
-    this.warning ="You must fill all te fields.";
-    this.showWarning = true;
-  }
-
-  showPasswordsDoNotMatchWarning(){
-    this.warning = "Passwords do not match";
-    this.showWarning = true;
-  }
-
-  areProfileFieldsReady(){
-    if(this.areBasicFieldsEmpty()){
-      this.showEmptyFieldsWarning();
-      return false;
-    }else{
-      if(this.isOnePasswordFieldEmptyButTheOtherOneNot()){
-        debugger;
-        this.showEmptyFieldsWarning();
-        return false
-      }
-      else{
-        if(this.doThePasswordsMatch()){
-          return true;
-        }else{
-          this.showPasswordsDoNotMatchWarning();
+    isOnePasswordFieldEmptyButTheOtherOneNot() {
+        if (ValidationService.errorInField(this.newPassword)) {
+            return (!ValidationService.errorInField(this.confirmPassword))
         }
-      }
+        else {
+            return (ValidationService.errorInField(this.confirmPassword))
+        }
     }
-  }
 
-  cancelEditProfile(){
-    this.editProfile = false;
-  }
-
-  finishEditProfile(){
-    if(this.areProfileFieldsReady()){
-      Globals.userInfo = this.editableUser;
-      this.editProfile = false;
+    doThePasswordsMatch() {
+        return (this.newPassword == this.confirmPassword);
     }
-  }
+
+    disableWarnings() {
+        this.showWarning = false;
+    }
+
+    showEmptyFieldsWarning() {
+        this.warning = "You must fill all te fields.";
+        this.showWarning = true;
+    }
+
+    showPasswordsDoNotMatchWarning() {
+        this.warning = "Passwords do not match";
+        this.showWarning = true;
+    }
+
+    enableSuccesfullyAlert(){
+        this.showSuccessAlert = true;
+
+        setTimeout(()=>{
+            this.showSuccessAlert = false;
+        },5000);
+    }
+
+    saveNewPassword() {
+        if (this.doThePasswordsMatch() && !ValidationService.errorInField(this.newPassword)) {
+            this.editableUser.password = this.newPassword;
+        }
+    }
+
+    areProfileFieldsReady() {
+        if (this.areBasicFieldsEmpty()) {
+            this.showEmptyFieldsWarning();
+            return false;
+        } else {
+            if (this.isOnePasswordFieldEmptyButTheOtherOneNot()) {
+                debugger;
+                this.showEmptyFieldsWarning();
+                return false
+            }
+            else {
+                if (this.doThePasswordsMatch()) {
+                    return true;
+                } else {
+                    this.showPasswordsDoNotMatchWarning();
+                }
+            }
+        }
+    }
+
+    cancelEditProfile() {
+        this.editProfile = false;
+    }
+
+    finishEditProfile() {
+        if (this.areProfileFieldsReady()) {
+            this.saveNewPassword();
+            this._userService.updateUser(this.editableUser);
+            this.editableUser = Globals.userInfo;
+            this.editProfile = false;
+            this.enableSuccesfullyAlert();
+        }
+    }
 
 }
