7956778586aef18f26c45d5611af34283182a2a9
DavidLio <liodavid0@gmail.com>
2017-12-22 01:17:19 -0700

    Se agrego la funcion para medir el nivel de la contraseña nueva

--- src/app/modals/reset-user/reset-user.component.ts
+++ src/app/modals/reset-user/reset-user.component.ts
@@ -1,18 +1,151 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Globals} from "../../statics/globals";
+import {forEach} from "@angular/router/src/utils/collection";
+import {User} from "../../interfaces/user";
 
 @Component({
-  selector: 'app-reset-user',
-  templateUrl: './reset-user.component.html',
-  styleUrls: ['./reset-user.component.scss']
+    selector: 'app-reset-user',
+    templateUrl: './reset-user.component.html',
+    styleUrls: ['./reset-user.component.scss']
 })
 export class ResetUserComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  securityQuestions: string[] = Globals.securityQuestions;
+    @ViewChild('securityLevel')
+    securityLevel: ElementRef;
 
-  ngOnInit() {
-  }
+    upperCaseLetters: string [] = [];
+    lowerCaseLetters: string [] = [];
+    specialCharacters: string [] = [];
 
+    uppercaseInPassword = false;
+    lowercaseInPassword = false;
+    specialCharacterInPassword = false;
+    acceptablePasswordLength = false;
+
+    editableUser: User = {} as User;
+
+    securityQuestions: string[] = Globals.securityQuestions;
+
+    passwordLevel: number = 0;
+    passwordStregth: string = "Weak";
+
+    ngOnInit() {
+        this.addLowerCaseToArray();
+        this.addSpecialCharactersToArray();
+        this.addUppercaseLetterToArray();
+    }
+
+
+    displayPasswordLevel() {
+        this.resetPasswordSecurityLevel()
+        this.validateCharactersInPassword();
+        this.validatePasswordLength();
+        this.sumPasswordLevel();
+        console.log(this.passwordLevel)
+
+        let color: string;
+        switch (this.passwordLevel) {
+            case 0:
+            case 1:
+                this.passwordStregth = "Weak";
+                color = "text-danger";
+                break;
+            case 2:
+                this.passwordStregth = "Good";
+                color = "text-warning";
+                break;
+            case 3:
+                this.passwordStregth = "Good";
+                color = "text-warning";
+                break;
+            case 4:
+                this.passwordStregth = "Strong";
+                color = "text-success";
+                break;
+        }
+
+        this.changePasswordLevelColor(color);
+    }
+
+    changePasswordLevelColor(color: string) {
+        this.securityLevel.nativeElement.removeAttribute('class');
+        this.securityLevel.nativeElement.setAttribute('class', color);
+    }
+
+    resetPasswordSecurityLevel(){
+        this.passwordLevel = 0;
+        this.uppercaseInPassword = this.lowercaseInPassword = this.specialCharacterInPassword = this.acceptablePasswordLength = false;
+    }
+    sumPasswordLevel(){
+        if(this.uppercaseInPassword) this.passwordLevel ++;
+        if(this.lowercaseInPassword) this.passwordLevel ++;
+        if(this.specialCharacterInPassword) this.passwordLevel ++;
+        if(this.acceptablePasswordLength) this.passwordLevel ++;
+    }
+
+    validateCharactersInPassword() {
+        this.validateUpperCase();
+        this.validateLowerCase()
+        this.validateSpecialCharacters();
+    }
+
+    validateUpperCase() {
+        for (let i = 0; i < this.upperCaseLetters.length; i++) {
+            if ((this.editableUser.password).includes(this.upperCaseLetters[i])) {
+                this.uppercaseInPassword = true;
+                break;
+            }
+        }
+    }
+
+    validateLowerCase() {
+        for (let i = 0; i < this.lowerCaseLetters.length; i++) {
+            if ((this.editableUser.password).includes(this.lowerCaseLetters[i])) {
+                this.lowercaseInPassword = true;
+                break;
+            }
+        }
+    }
+
+    validateSpecialCharacters() {
+        for (let i = 0; i < this.specialCharacters.length; i++) {
+            if ((this.editableUser.password).includes(this.specialCharacters[i])) {
+                this.specialCharacterInPassword = true;
+                break;
+            }
+        }
+    }
+
+    validatePasswordLength() {
+        if (this.editableUser.password.length > 8) {
+            this.acceptablePasswordLength = true;
+        }
+
+    }
+
+    addUppercaseLetterToArray() {
+        for (let i = 65; i <= 90; i++) {
+            this.upperCaseLetters.push(String.fromCharCode(i));
+        }
+    }
+
+    addLowerCaseToArray() {
+        for (let i = 97; i <= 122; i++) {
+            this.lowerCaseLetters.push(String.fromCharCode(i));
+        }
+    }
+
+    addSpecialCharactersToArray() {
+        this.specialCharacters.push(String.fromCharCode(36)) //$
+        this.specialCharacters.push(String.fromCharCode(64)) //@
+        this.specialCharacters.push(String.fromCharCode(33)) //!
+        this.specialCharacters.push(String.fromCharCode(37)) //%
+        this.specialCharacters.push(String.fromCharCode(34)) //*
+        this.specialCharacters.push(String.fromCharCode(35)) //#
+        this.specialCharacters.push(String.fromCharCode(63)) //?
+        this.specialCharacters.push(String.fromCharCode(38)) //&
+    }
 }
