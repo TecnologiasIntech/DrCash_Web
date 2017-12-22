29c94e42a45e388a0930d810b1c19597db4d813b
DavidLio <liodavid0@gmail.com>
2017-12-21 18:25:39 -0700

    Se añadio la funcion para agregar usuarios y se agregaron las validaciones de los campos, falta validacion de Username repetido

--- src/app/modals/sign-up/sign-up.component.ts
+++ src/app/modals/sign-up/sign-up.component.ts
@@ -1,7 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Globals} from "../../statics/globals";
 import {DateService} from "../../services/date.service";
 import {User} from "../../interfaces/user";
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {ValidationService} from "../../services/validation.service";
+import {UserService} from "../../services/user.service";
 
 @Component({
     selector: 'app-sign-up',
@@ -10,12 +13,82 @@ import {User} from "../../interfaces/user";
 })
 export class SignUpComponent implements OnInit {
 
-    newUser:User;
+    newUser:User = {} as User;
 
-    constructor() {
+    constructor(private _activeModal: NgbActiveModal,
+                private _userService: UserService) {
     }
+    @ViewChild('username')
+    username: ElementRef;
+    @ViewChild('firstName')
+    firstName: ElementRef;
+    @ViewChild('lastName')
+    lastName: ElementRef;
+    @ViewChild('email')
+    email: ElementRef;
+
+    securityLevel:string = "0";
 
     ngOnInit() {
+    }
+
+    saveUser(){
+        if(!this.areEmptyFields()){
+            this.updateSecurityLevel();
+            this.setActiveUser();
+            this.setPasswordReset();
+            this.setUserClinic();
+            this._userService.updateUser(this.newUser);
+            this.resetNewUser();
+
+        }
+    }
+
+    setUserClinic(){
+        this.newUser.clinic = Globals.userInfo.clinic;
+    }
+
+    updateSecurityLevel(){
+        this.newUser.securityLevel = parseInt(this.securityLevel);
+    }
+
+    setActiveUser(){
+        this.newUser.activeAccount = true;
+    }
+
+    setPasswordReset(){
+        this.newUser.passwordReset = true;
+    }
+
+    resetNewUser(){
+        this.newUser = {} as User;
+    }
+
+    areEmptyFields(){
+        if(ValidationService.errorInField(this.newUser.username)){
+            this.username.nativeElement.focus();
+            return true;
+        }
+        if(ValidationService.errorInField(this.newUser.firstName)){
+            this.firstName.nativeElement.focus();
+            return true;
+        }
+        if(ValidationService.errorInField(this.newUser.lastName)){
+            this.lastName.nativeElement.focus();
+            return true;
+        }
+        if(ValidationService.errorInField(this.newUser.email)){
+            this.email.nativeElement.focus();
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    closeModal(){
+        this._activeModal.close();
     }
 
     loadUserDefaultData(){
