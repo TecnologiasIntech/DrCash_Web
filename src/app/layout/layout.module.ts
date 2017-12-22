8cb33ae8b69742fa7aedb52def14ad829ecf9976
DavidLio <liodavid0@gmail.com>
2017-12-21 23:31:11 -0700

    Se creo el componente ResetUser

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -46,6 +46,7 @@ import {ValidationService} from "../services/validation.service";
 import {InitialCashComponent} from "../modals/initial-cash/initial-cash.component";
 import {Globals} from "../statics/globals";
 import {DateService} from "../services/date.service";
+import {ResetUserComponent} from "../modals/reset-user/reset-user.component";
 
 const PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     suppressScrollX: true
@@ -78,7 +79,8 @@ const PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
         SmtpComponent,
         SignUpComponent,
         LoginComponent,
-        InitialCashComponent
+        InitialCashComponent,
+        ResetUserComponent
     ],
     imports: [
         CommonModule,
@@ -117,7 +119,8 @@ const PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
         SignUpComponent,
         UpdateTransactionComponent,
         AuthorizationComponent,
-        InitialCashComponent
+        InitialCashComponent,
+        ResetUserComponent
 
     ]
 })
---
+++ src/app/modals/reset-user/reset-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-user',
+  templateUrl: './reset-user.component.html',
+  styleUrls: ['./reset-user.component.scss']
+})
+export class ResetUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
