67ad331bc79974b733d976d6d6b25c038a949d10
Carlos Alatorre <carlos_alatorre_95@hotmail.com>
2017-12-23 00:47:51 -0700

    Funcionalidad para mostrar las transacciones buscadas

--- src/app/pages/daily-transactions/daily-transactions.component.ts
+++ src/app/pages/daily-transactions/daily-transactions.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Transaction} from "../../interfaces/transaction";
+import {TransactionService} from "../../services/transaction.service";
+import {ValidationService} from "../../services/validation.service";
+import {TRANSACTIONTYPE} from "../../enums/enums";
 
 @Component({
-  selector: 'app-daily-transactions',
-  templateUrl: './daily-transactions.component.html',
-  styleUrls: ['./daily-transactions.component.scss']
+    selector: 'app-daily-transactions',
+    templateUrl: './daily-transactions.component.html',
+    styleUrls: ['./daily-transactions.component.scss']
 })
 export class DailyTransactionsComponent implements OnInit {
 
-  constructor() { }
+    transactions: Transaction[];
+    patientName: string;
+    comment: string;
+    transactionNumber: number;
+    dateFrom: any;
+    dateTo: any;
 
-  ngOnInit() {
-  }
+    constructor(private _transactionService: TransactionService) {
+        this.cleanFields()
+    }
+
+    ngOnInit() {
+    }
+
+    searchTransactions() {
+        this.validateDateFields();
+        this._transactionService.searchDailyTransactions(this.transactionNumber, this.comment, this.patientName, this.dateFrom, this.dateTo)
+            .then((response: Transaction[]) => {
+                this.transactions = response;
+            })
+    }
+
+    validateDateFields() {
+        if (!ValidationService.errorInField(this.dateFrom)) this.dateFrom = parseInt(this.dateFrom);
+        if (!ValidationService.errorInField(this.dateTo)) this.dateTo = parseInt(this.dateTo);
+    }
+
+    cleanFields() {
+        this.patientName = null;
+        this.comment = null;
+        this.transactionNumber = null;
+        this.dateFrom = null;
+        this.dateTo = null;
+    }
+
+    getTypeTransaction(type:number){
+        switch (type){
+            case TRANSACTIONTYPE.INITIALCASH:
+                return 'Initial Cash';
+
+            case TRANSACTIONTYPE.CASHIN:
+                return 'Cash In';
+
+            case TRANSACTIONTYPE.CASHOUT:
+                return 'Cash Out';
+
+            case TRANSACTIONTYPE.REFUND:
+                return 'Refund';
+        }
+    }
 
 }
