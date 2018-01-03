5511d2c9bc523385b80e1f7cb8f4983414b71d4d
DavidLio <liodavid0@gmail.com>
2018-01-02 22:45:42 -0700

    Se hicieron correcciones en el diseño y se agregaron validaciones

--- src/app/pages/general/general.component.ts
+++ src/app/pages/general/general.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import {AngularFireDatabase, AngularFireList} from "angularfire2/database";
 import {Globals} from "../../statics/globals";
 import * as firebase from "firebase";
@@ -6,6 +6,7 @@ import Settings = firebase.firestore.Settings;
 import {Setting} from "../../interfaces/setting";
 import {SettingService} from "../../services/setting.service";
 import {settings} from "cluster";
+import {ValidationService} from "../../services/validation.service";
 
 @Component({
   selector: 'app-general',
@@ -19,15 +20,13 @@ export class GeneralComponent implements OnInit {
           .then((response: Setting) => {
               this.settings = response;
           })
+      this.parseBooleanToString();
 
-      if(this.settings.leaveMoneyInRegister)this.leaveMoneyInRegister = "true";
-      if(!this.settings.leaveMoneyInRegister)this.leaveMoneyInRegister = "false";
-      if(this.settings.sendPasswordThroughEmail)this.sendPassword = "true";
-      if(!this.settings.sendPasswordThroughEmail)this.sendPassword = "false";
-      if(this.settings.useDefaultPassword)this.useDefaultPassword = "true";
-      if(!this.settings.useDefaultPassword)this.useDefaultPassword = "false";
 
   }
+
+    @ViewChild('defaultPasswordInput')
+    defaultPasswordInput: ElementRef;
 
   settings: Setting = {} as Setting;
 
@@ -35,20 +34,52 @@ export class GeneralComponent implements OnInit {
   sendPassword: string;
   useDefaultPassword: string;
   ngOnInit() {
-
+      this.enableOrDisableDefaultPasswordInput();
   }
 
   saveSettings(){
+      if(this.isDefaultPasswordEnabledAndFilled()){
+          this.prepareSettings();
+          this._settingService.setSettings(this.settings);
+      }
+  }
+
+  prepareSettings(){
       this.settings.leaveMoneyInRegister = this.parseBoolean(this.leaveMoneyInRegister);
       this.settings.sendPasswordThroughEmail = this.parseBoolean(this.sendPassword);
       this.settings.useDefaultPassword = this.parseBoolean(this.useDefaultPassword);
-      this._settingService.setSettings(this.settings);
-
+      this.settings.idleTime = parseInt(this.settings.idleTime.toLocaleString());
   }
 
   parseBoolean(text:string){
       if(text.toLowerCase() == "true") return true;
       if(text.toLowerCase() == "false") return false;
+  }
+
+  parseBooleanToString(){
+      if(this.settings.leaveMoneyInRegister)this.leaveMoneyInRegister = "true";
+      if(!this.settings.leaveMoneyInRegister)this.leaveMoneyInRegister = "false";
+      if(this.settings.sendPasswordThroughEmail)this.sendPassword = "true";
+      if(!this.settings.sendPasswordThroughEmail)this.sendPassword = "false";
+      if(this.settings.useDefaultPassword)this.useDefaultPassword = "true";
+      if(!this.settings.useDefaultPassword)this.useDefaultPassword = "false";
+  }
+
+  enableOrDisableDefaultPasswordInput(){
+      console.log(this.useDefaultPassword)
+          if (this.useDefaultPassword == "true") {
+              this.defaultPasswordInput.nativeElement.removeAttribute('disabled');
+          } else {
+              this.defaultPasswordInput.nativeElement.setAttribute('disabled', 'disabled');
+          }
+  }
+
+  isDefaultPasswordEnabledAndFilled(){
+      if(this.useDefaultPassword == "true" && ValidationService.errorInField(this.settings.defaultPassword)){
+          this.defaultPasswordInput.nativeElement.focus();
+          return false;
+      }
+      return true;
   }
 
     restrictNumeric(e) {
